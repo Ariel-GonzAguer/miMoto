a78f9072fa9741b46d4067404ddb2e736d36dee0
Ariel-GonzAguer <ariegonzaguer@gmail.com>
2024-11-04 18:36:13 -0600

    primer commit. inicio proyecto

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <section
+      style={{
+        margin: "0 auto",
+        textAlign: "center",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>miMoto</h1>
+    </section>
+  );
+}
